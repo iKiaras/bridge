7d85f4515c13aea072d3214a6399caca08338273
Ioannis Kiaras <ikiaras@voiceweb.eu>
2017-03-14 18:07:23 +0200

    Minor text input improvments

--- app/src/main/java/orasis/birdge/MainMenu.java
+++ app/src/main/java/orasis/birdge/MainMenu.java
@@ -1,14 +1,68 @@
 package orasis.birdge;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainMenu extends Activity {
 
+    private TextView bridgeText;
+    private TextToSpeech speech;
+    private EditText textInput;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    public static final int speech_not_supported=0x7f060004;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        textInput = (EditText) findViewById(R.id.editTextInput);
+
+    }
+
+    @SuppressWarnings("ResourceType")
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+//        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+//                getString(speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                   // lastCommand = result.get(0);
+                  //  checkMove(lastCommand);
+                }
+                break;
+            }
+
+        }
     }
 }
